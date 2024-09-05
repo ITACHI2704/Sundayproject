52303ec2897783d82ff2f4b9bffb22ed7f6c3f9f
krishan kumar <krishankumar39848@gmail.com>
2024-09-05 14:40:05 +0530

    contact page and about page

--- script.js
+++ script.js
@@ -15,20 +15,29 @@ cut.addEventListener("click", ()=>{
 
     // .............................
 
-
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxrZsjAEtQH_-OheTkdNsbnWc3DuYBKelzjTQO0eRBRBn7U5S9HUZPgTbpdUB_hoZpCMg/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response =>{
-      msg.innerHTML="Message sent sucessfully"
-      setTimeout(function (){
-        msg.innerHTML=""
-      },2000)
-      form.reset()
+    let tablinks = document.getElementsByClassName("tab-links")
+    let tabcontents = document.getElementsByClassName("tab-content")
+    
+    tablinks.addEventListener("click",(name)=>{
+    return name ;
     })
-    .catch(error => console.error('Error!', error.message))
-})
+  
+    function opentab (tabname){
+      for(tablink of tablinks){
+        tablink.classList.remove("active-links")
+      }
+      for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+          }
+      event.currentTarget.classList.add("active-links")
+      document.getElementById(tabname).classList.add("active-tab")
+    }
+
+
+
+
+
+
+
+
+
